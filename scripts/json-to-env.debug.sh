7580d240f2a41f65396c8e9a381acc1ac075ef96
dayland <dayland@microsoft.com>
2023-04-20 13:56:38 +0100

    Setup launch.json to debug webapp

---
+++ scripts/json-to-env.debug.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+set -e
+
+jq -r  '
+    .properties.outputs |
+    [
+        {
+            "path": "azurE_LOCATION",
+            "env_var": "LOCATION"
+        },
+        {
+            "path": "azurE_SEARCH_INDEX",
+            "env_var": "AZURE_SEARCH_INDEX"
+        },
+        {
+            "path": "azurE_SEARCH_SERVICE",
+            "env_var": "AZURE_SEARCH_SERVICE"
+        },
+        {
+            "path": "azurE_STORAGE_ACCOUNT",
+            "env_var": "AZURE_BLOB_STORAGE_ACCOUNT"
+        },
+        {
+            "path": "azurE_STORAGE_CONTAINER",
+            "env_var": "AZURE_BLOB_STORAGE_CONTAINER"
+        },
+        {
+            "path": "azurE_OPENAI_SERVICE",
+            "env_var": "AZURE_OPENAI_SERVICE"
+        },
+        {
+            "path": "backenD_URI",
+            "env_var": "AZURE_WEBAPP_URI"
+        },
+        {
+            "path": "backenD_NAME",
+            "env_var": "AZURE_WEBAPP_NAME"
+        },
+        {
+            "path": "resourcE_GROUP_NAME",
+            "env_var": "RESOURCE_GROUP_NAME"
+        },
+        {
+            "path": "coG_SERVICES_FOR_SEARCH_KEY",
+            "env_var": "COGNITIVE_SERVICES_KEY"
+        },
+        {
+            "path": "azurE_OPENAI_GPT_DEPLOYMENT",
+            "env_var": "AZURE_OPENAI_GPT_DEPLOYMENT"
+        },
+        {
+            "path": "azurE_OPENAI_CHAT_GPT_DEPLOYMENT",
+            "env_var": "AZURE_OPENAI_CHATGPT_DEPLOYMENT"
+        },
+        {
+            "path": "azurE_OPENAI_SERVICE_KEY",
+            "env_var": "AZURE_OPENAI_SERVICE_KEY"
+        }
+    ]
+        as $env_vars_to_extract
+    |
+    with_entries(
+        select (
+            .key as $a
+            |
+            any( $env_vars_to_extract[]; .path == $a)
+        )
+        |
+        .key |= . as $old_key | ($env_vars_to_extract[] | select (.path == $old_key) | .env_var)
+    )
+    |
+    to_entries
+    | 
+    map("\(.key)=\"\(.value.value)\"")
+    |
+    .[]
+    ' | sed "s/\"/'/g" # replace double quote with single quote to handle special chars

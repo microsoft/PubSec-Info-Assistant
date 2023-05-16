246ce86bfd76e1654fa1f2260701a5c43a740c84
dayland <dayland@microsoft.com>
2023-05-16 14:03:38 +0100

    Update webapp deploy to async and increase timeout

--- scripts/deploy-webapp.sh
+++ scripts/deploy-webapp.sh
@@ -14,4 +14,4 @@ end=`date -u -d "3 years" '+%Y-%m-%dT%H:%MZ'`
 cd $BINARIES_OUTPUT_PATH
 file=$(az storage blob upload --account-name $AZURE_BLOB_STORAGE_ACCOUNT --account-key $AZURE_BLOB_STORAGE_KEY --container-name website --name webapp.zip --file webapp.zip --overwrite)
 sas=$(az storage blob generate-sas --account-name $AZURE_BLOB_STORAGE_ACCOUNT --account-key $AZURE_BLOB_STORAGE_KEY --container-name website --name webapp.zip --permissions r --expiry $end --output tsv)
-az webapp deploy --name $AZURE_WEBAPP_NAME --resource-group $RESOURCE_GROUP_NAME --type zip --src-url "https://$AZURE_BLOB_STORAGE_ACCOUNT.blob.core.windows.net/website/webapp.zip?$sas" --timeout 300000
+az webapp deploy --name $AZURE_WEBAPP_NAME --resource-group $RESOURCE_GROUP_NAME --type zip --src-url "https://$AZURE_BLOB_STORAGE_ACCOUNT.blob.core.windows.net/website/webapp.zip?$sas" --async true --timeout 600000

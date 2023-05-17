60a453849549822f463a4fccb95f808e0c6266c1
dayland <dayland@microsoft.com>
2023-05-17 21:53:52 +0100

    Updated webapp deploy to use kudu api with auth

--- scripts/deploy-webapp.sh
+++ scripts/deploy-webapp.sh
@@ -12,6 +12,18 @@ BINARIES_OUTPUT_PATH="${DIR}/../artifacts/build/"
 end=`date -u -d "3 years" '+%Y-%m-%dT%H:%MZ'`
 
 cd $BINARIES_OUTPUT_PATH
+
+# upload the zip file to blob storage
 file=$(az storage blob upload --account-name $AZURE_BLOB_STORAGE_ACCOUNT --account-key $AZURE_BLOB_STORAGE_KEY --container-name website --name webapp.zip --file webapp.zip --overwrite)
+# generate a SAS token for the zip file
 sas=$(az storage blob generate-sas --account-name $AZURE_BLOB_STORAGE_ACCOUNT --account-key $AZURE_BLOB_STORAGE_KEY --container-name website --name webapp.zip --permissions r --expiry $end --output tsv)
-az webapp deploy --name $AZURE_WEBAPP_NAME --resource-group $RESOURCE_GROUP_NAME --type zip --src-url "https://$AZURE_BLOB_STORAGE_ACCOUNT.blob.core.windows.net/website/webapp.zip?$sas" --async true --timeout 600000 --clean true --debug --verbose
+
+# get the publishing profile for the webapp
+username=$(az webapp deployment list-publishing-profiles --resource-group infoasst-dayland --name infoasst-web-jg5so --query "[?publishMethod=='ZipDeploy'].userName" --output tsv)
+pwd=$(az webapp deployment list-publishing-profiles --resource-group infoasst-dayland --name infoasst-web-jg5so --query "[?publishMethod=='ZipDeploy'].userPWD" --output tsv)
+publishUrl=$(az webapp deployment list-publishing-profiles --resource-group infoasst-dayland --name infoasst-web-jg5so --query "[?publishMethod=='ZipDeploy'].publishUrl" --output tsv)
+
+# deploy the zip file to the webapp
+curl -X POST -u $username:$pwd https://$publishUrl/api/zipdeploy -d "{\"packageUri\": \"https://$AZURE_BLOB_STORAGE_ACCOUNT.blob.core.windows.net/website/webapp.zip?$sas\"}" -H "Content-Type: application/json"
+
+echo "Webapp deployed successfully"

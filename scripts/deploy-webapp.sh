b0227925b3211bb3ad344ff764710c104800906d
dayland <dayland@microsoft.com>
2023-05-25 02:03:42 +0100

    Fix logic in test for Azure CLI min version

--- scripts/deploy-webapp.sh
+++ scripts/deploy-webapp.sh
@@ -17,15 +17,15 @@ cd $BINARIES_OUTPUT_PATH
 version=$(az version --output tsv --query '"azure-cli"')
 version_parts=(${version//./ })
 if [ ${version_parts[0]} -lt 2 ]; then
-    ehco "Azure CLI version 2.48.1 or higher is required for webapp deployment"
+    ehco "Azure CLI version 2.48.1 or higher is required for webapp deployment. Please run 'az upgrade' to upgrade your Azure CLI version."
     exit 1
 else
-    if [ ${version_parts[0]} -eq 2 ] & [ ${version_parts[1]} -lt 48 ]; then
-        echo "Azure CLI version 2.48.1 or higher is required for webapp deployment"
+    if [ ${version_parts[0]} -le 2 ] && [ ${version_parts[1]} -lt 48 ]; then
+        echo "Azure CLI version 2.48.1 or higher is required for webapp deployment. Please run 'az upgrade' to upgrade your Azure CLI version."
         exit 1
     else
-        if [ ${version_parts[0]} -eq 2 ] & [ ${version_parts[1]} -eq 48 ] &  [ ${version_parts[2]} -lt 1 ]; then
-            echo "Azure CLI version 2.48.1 or later is required to run this script"
+        if [ ${version_parts[0]} -le 2 ] && [ ${version_parts[1]} -le 48 ] && [ ${version_parts[2]} -lt 1 ]; then
+            echo "Azure CLI version 2.48.1 or later is required to run this script. Please run 'az upgrade' to upgrade your Azure CLI version."
             exit 1
         fi
     fi

3105a2f76292df262103d50ca533b43bf9b22b35
dayland <48474707+dayland@users.noreply.github.com>
2023-06-05 13:38:42 +0100

    Fix parsing errors with citations and fuqs in ux

--- app/frontend/src/components/Answer/AnswerParser.tsx
+++ app/frontend/src/components/Answer/AnswerParser.tsx
@@ -12,7 +12,7 @@ export function parseAnswerToHtml(answer: string, citation_lookup: {}, onCitatio
     const followupQuestions: string[] = [];
 
     // Extract any follow-up questions that might be in the answer
-    let parsedAnswer = answer.replace(/<<([^>>]+)>>/g, (match, content) => {
+    let parsedAnswer = answer.replace(/<<<([^>>>]+)>>>/g, (match, content) => {
         followupQuestions.push(content);
         return "";
     });
@@ -29,28 +29,33 @@ export function parseAnswerToHtml(answer: string, citation_lookup: {}, onCitatio
             return part;
         } else {
             // Odd parts are citations as the "FileX" moniker
-            let citationIndex: number;
-            if (citations.indexOf((citation_lookup as any)[part]) !== -1) {
-                citationIndex = citations.indexOf((citation_lookup as any)[part]) + 1;
-            } else {
-                // splitting the full file path from citation_lookup into an array and then slicing it to get the folders, file name, and extension 
-                // the first 4 elements of the full file path are the "https:", "", "blob storaage url", and "container name" which are not needed in the display
-                if(typeof (citation_lookup as any)[part] === 'undefined') {
-                    part = part.split(".")[0];
-                }
-                citations.push((citation_lookup as any)[part].split("/").slice(4).join("/"));
-                citationIndex = citations.length;
+            if ( typeof((citation_lookup as any)[part]) === "undefined") {
+                // if the citation reference provided by the OpenAI response does not match a key in the citation_lookup object
+                // then return an empty string to avoid a crash or blank citation
+                console.log("citation not found for: " + part)
+                return "";
             }
+            else {
+                let citationIndex: number;
+                if (citations.indexOf((citation_lookup as any)[part]) !== -1) {
+                    citationIndex = citations.indexOf((citation_lookup as any)[part]) + 1;
+                } else {
+                    // splitting the full file path from citation_lookup into an array and then slicing it to get the folders, file name, and extension 
+                    // the first 4 elements of the full file path are the "https:", "", "blob storaage url", and "container name" which are not needed in the display
+                    citations.push((citation_lookup as any)[part].split("/").slice(4).join("/"));
+                    citationIndex = citations.length;
+                }
 
-            const path = getCitationFilePath((citation_lookup as any)[part]);
+                const path = getCitationFilePath((citation_lookup as any)[part]);
 
-            return renderToStaticMarkup(
-                // splitting the full file path from citation_lookup into an array and then slicing it to get the folders, file name, and extension 
-                // the first 4 elements of the full file path are the "https:", "", "blob storaage url", and "container name" which are not needed in the display
-                <a className="supContainer" title={(citation_lookup as any)[part].split("/").slice(4).join("/")} onClick={() => onCitationClicked(path)}>
-                    <sup>{citationIndex}</sup>
-                </a>
-            );
+                return renderToStaticMarkup(
+                    // splitting the full file path from citation_lookup into an array and then slicing it to get the folders, file name, and extension 
+                    // the first 4 elements of the full file path are the "https:", "", "blob storaage url", and "container name" which are not needed in the display
+                    <a className="supContainer" title={(citation_lookup as any)[part].split("/").slice(4).join("/")} onClick={() => onCitationClicked(path)}>
+                        <sup>{citationIndex}</sup>
+                    </a>
+                );
+            }
         }
     });
 

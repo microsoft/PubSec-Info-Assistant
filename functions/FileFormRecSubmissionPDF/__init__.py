f61b48cb6ede37311a5c1e22b1a955552cd09ba6
Liam Wilkinson <lwilkinson@microsoft.com>
2023-07-12 15:00:34 +0100

    Changed to GA FR API Vers and fixed error log

--- functions/FileFormRecSubmissionPDF/__init__.py
+++ functions/FileFormRecSubmissionPDF/__init__.py
@@ -1,19 +1,21 @@
-import logging
-import azure.functions as func
-from azure.storage.blob import generate_blob_sas
-from azure.storage.queue import QueueClient, TextBase64EncodePolicy
+import json
 import logging
 import os
-import json
-import requests
-from shared_code.status_log import StatusLog, State, StatusClassification
-from shared_code.utilities import Utilities
 import random
 
+import azure.functions as func
+import requests
+from azure.storage.queue import QueueClient, TextBase64EncodePolicy
+from shared_code.status_log import State, StatusClassification, StatusLog
+from shared_code.utilities import Utilities
 
 azure_blob_storage_account = os.environ["BLOB_STORAGE_ACCOUNT"]
-azure_blob_drop_storage_container = os.environ["BLOB_STORAGE_ACCOUNT_UPLOAD_CONTAINER_NAME"]
-azure_blob_content_storage_container = os.environ["BLOB_STORAGE_ACCOUNT_OUTPUT_CONTAINER_NAME"]
+azure_blob_drop_storage_container = os.environ[
+    "BLOB_STORAGE_ACCOUNT_UPLOAD_CONTAINER_NAME"
+]
+azure_blob_content_storage_container = os.environ[
+    "BLOB_STORAGE_ACCOUNT_OUTPUT_CONTAINER_NAME"
+]
 azure_blob_storage_key = os.environ["BLOB_STORAGE_ACCOUNT_KEY"]
 azure_blob_connection_string = os.environ["BLOB_CONNECTION_STRING"]
 cosmosdb_url = os.environ["COSMOSDB_URL"]
@@ -27,80 +29,145 @@ FR_key = os.environ["AZURE_FORM_RECOGNIZER_KEY"]
 api_version = os.environ["FR_API_VERSION"]
 
 
-statusLog = StatusLog(cosmosdb_url, cosmosdb_key, cosmosdb_database_name, cosmosdb_container_name)
-utilities = Utilities(azure_blob_storage_account, azure_blob_drop_storage_container, azure_blob_content_storage_container, azure_blob_storage_key)
+statusLog = StatusLog(
+    cosmosdb_url, cosmosdb_key, cosmosdb_database_name, cosmosdb_container_name
+)
+utilities = Utilities(
+    azure_blob_storage_account,
+    azure_blob_drop_storage_container,
+    azure_blob_content_storage_container,
+    azure_blob_storage_key,
+)
 FR_MODEL = "prebuilt-layout"
-MAX_REQUEUE_COUNT = 5   #max times we will retry the submission
-POLL_QUEUE_SUBMIT_BACKOFF = 60    # Hold for n seconds to give FR time to process the file
-PDF_SUBMIT_QUEUE_BACKOFF = 60 
+MAX_REQUEUE_COUNT = 5  # max times we will retry the submission
+POLL_QUEUE_SUBMIT_BACKOFF = 60  # Hold for n seconds to give FR time to process the file
+PDF_SUBMIT_QUEUE_BACKOFF = 60
 function_name = "FileFormRecSubmissionPDF"
 
 
-
 def main(msg: func.QueueMessage) -> None:
+    message_body = msg.get_body().decode("utf-8")
+    message_json = json.loads(message_body)
+    blob_path = message_json["blob_name"]
     try:
-        logging.info('Python queue trigger function processed a queue item: %s',
-                    msg.get_body().decode('utf-8'))
+        logging.info(
+            "Python queue trigger function processed a queue item: %s",
+            msg.get_body().decode("utf-8"),
+        )
 
         # Receive message from the queue
-        message_body = msg.get_body().decode('utf-8')
-        message_json = json.loads(message_body)
-        blob_path =  message_json['blob_name']
-        queued_count =  message_json['submit_queued_count']
-        statusLog.upsert_document(blob_path, f'{function_name} - Received message from pdf-submit-queue ', StatusClassification.DEBUG, State.PROCESSING)
-        statusLog.upsert_document(blob_path, f'{function_name} - Submitting to Form Recognizer', StatusClassification.INFO)
+
+        queued_count = message_json["submit_queued_count"]
+        statusLog.upsert_document(
+            blob_path,
+            f"{function_name} - Received message from pdf-submit-queue ",
+            StatusClassification.DEBUG,
+            State.PROCESSING,
+        )
+        statusLog.upsert_document(
+            blob_path,
+            f"{function_name} - Submitting to Form Recognizer",
+            StatusClassification.INFO,
+        )
         # construct blob url
         blob_path_plus_sas = utilities.get_blob_and_sas(blob_path)
-        statusLog.upsert_document(blob_path, f'{function_name} - SAS token generated', StatusClassification.DEBUG)
+        statusLog.upsert_document(
+            blob_path,
+            f"{function_name} - SAS token generated",
+            StatusClassification.DEBUG,
+        )
 
         # Construct and submmit the message to FR
         headers = {
-            'Content-Type': 'application/json',
-            'Ocp-Apim-Subscription-Key': FR_key
+            "Content-Type": "application/json",
+            "Ocp-Apim-Subscription-Key": FR_key,
         }
 
-        params = {
-            'api-version': api_version
-        }
-        
-        body = {
-            "urlSource": blob_path_plus_sas
-        }
+        params = {"api-version": api_version}
+
+        body = {"urlSource": blob_path_plus_sas}
         url = f"{endpoint}formrecognizer/documentModels/{FR_MODEL}:analyze"
-    
+
+        logging.info(f"Submitting to FR with url: {url}")
+
         # Send the HTTP POST request with headers, query parameters, and request body
         response = requests.post(url, headers=headers, params=params, json=body)
 
         # Check if the request was successful (status code 200)
         if response.status_code == 202:
             # Successfully submitted
-            statusLog.upsert_document(blob_path, f'{function_name} - PDF submitted to FR successfully', StatusClassification.DEBUG) 
-            resultId = response.headers.get("apim-request-id")  
-            message_json['FR_resultId'] = resultId  
-            message_json['polling_queue_count'] = 1     
-            queue_client = QueueClient.from_connection_string(azure_blob_connection_string, queue_name=pdf_polling_queue, message_encode_policy=TextBase64EncodePolicy())    
-            message_json_str = json.dumps(message_json)    
-            queue_client.send_message(message_json_str, visibility_timeout = POLL_QUEUE_SUBMIT_BACKOFF)  
-            statusLog.upsert_document(blob_path, f'{function_name} - message sent to pdf-polling-queue. Visible in {POLL_QUEUE_SUBMIT_BACKOFF} seconds. FR Result ID is {resultId}', StatusClassification.DEBUG, State.QUEUED) 
+            statusLog.upsert_document(
+                blob_path,
+                f"{function_name} - PDF submitted to FR successfully",
+                StatusClassification.DEBUG,
+            )
+            resultId = response.headers.get("apim-request-id")
+            message_json["FR_resultId"] = resultId
+            message_json["polling_queue_count"] = 1
+            queue_client = QueueClient.from_connection_string(
+                azure_blob_connection_string,
+                queue_name=pdf_polling_queue,
+                message_encode_policy=TextBase64EncodePolicy(),
+            )
+            message_json_str = json.dumps(message_json)
+            queue_client.send_message(
+                message_json_str, visibility_timeout=POLL_QUEUE_SUBMIT_BACKOFF
+            )
+            statusLog.upsert_document(
+                blob_path,
+                f"{function_name} - message sent to pdf-polling-queue. Visible in {POLL_QUEUE_SUBMIT_BACKOFF} seconds. FR Result ID is {resultId}",
+                StatusClassification.DEBUG,
+                State.QUEUED,
+            )
 
         elif response.status_code == 429:
             # throttled, so requeue with random backoff seconds to mitigate throttling, unless it has hit the max tries
             if queued_count < MAX_REQUEUE_COUNT:
-                max_seconds = PDF_SUBMIT_QUEUE_BACKOFF * (queued_count ** 2)
-                backoff =  random.randint(PDF_SUBMIT_QUEUE_BACKOFF * queued_count, max_seconds)
+                max_seconds = PDF_SUBMIT_QUEUE_BACKOFF * (queued_count**2)
+                backoff = random.randint(
+                    PDF_SUBMIT_QUEUE_BACKOFF * queued_count, max_seconds
+                )
                 queued_count += 1
-                message_json['queued_count'] = queued_count
-                statusLog.upsert_document(blob_path, f"{function_name} - Throttled on PDF submission to FR, requeuing. Back off of {backoff} seconds", StatusClassification.DEBUG) 
-                queue_client = QueueClient.from_connection_string(azure_blob_connection_string, queue_name=pdf_submit_queue, message_encode_policy=TextBase64EncodePolicy())   
-                message_json_str = json.dumps(message_json)  
+                message_json["queued_count"] = queued_count
+                statusLog.upsert_document(
+                    blob_path,
+                    f"{function_name} - Throttled on PDF submission to FR, requeuing. Back off of {backoff} seconds",
+                    StatusClassification.DEBUG,
+                )
+                queue_client = QueueClient.from_connection_string(
+                    azure_blob_connection_string,
+                    queue_name=pdf_submit_queue,
+                    message_encode_policy=TextBase64EncodePolicy(),
+                )
+                message_json_str = json.dumps(message_json)
                 queue_client.send_message(message_json_str, visibility_timeout=backoff)
-                statusLog.upsert_document(blob_path, f'{function_name} - message sent to pdf-submit-queue. Visible in {backoff} seconds.', StatusClassification.DEBUG, State.QUEUED) 
+                statusLog.upsert_document(
+                    blob_path,
+                    f"{function_name} - message sent to pdf-submit-queue. Visible in {backoff} seconds.",
+                    StatusClassification.DEBUG,
+                    State.QUEUED,
+                )
             else:
-                statusLog.upsert_document(blob_path, f'{function_name} - maximum submissions to FR reached', StatusClassification.ERROR, State.ERROR) 
+                statusLog.upsert_document(
+                    blob_path,
+                    f"{function_name} - maximum submissions to FR reached",
+                    StatusClassification.ERROR,
+                    State.ERROR,
+                )
 
         else:
             # general error occurred
-            statusLog.upsert_document(blob_path, f'{function_name} - Error on PDF submission to FR - {response.status_code} - {response.message}', StatusClassification.ERROR, State.ERROR) 
-            
-    except Exception as e:
-        statusLog.upsert_document(blob_path, f"{function_name} - An error occurred - {str(e)}", StatusClassification.ERROR, State.ERROR)
+            statusLog.upsert_document(
+                blob_path,
+                f"{function_name} - Error on PDF submission to FR - {response.status_code} - {response.reason}",
+                StatusClassification.ERROR,
+                State.ERROR,
+            )
+
+    except Exception as error:
+        statusLog.upsert_document(
+            blob_path,
+            f"{function_name} - An error occurred - {str(error)}",
+            StatusClassification.ERROR,
+            State.ERROR,
+        )

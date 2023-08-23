c3cc44f37ebf032eecf25459011223c7abc3474d
dayland <48474707+dayland@users.noreply.github.com>
2023-08-23 23:42:53 +0100

    fixing CI/CD build issues

--- .devcontainer/scripts/non-root-user.sh
+++ .devcontainer/scripts/non-root-user.sh
@@ -13,7 +13,6 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-
 # If in automatic mode, determine if a user already exists, if not use vscode
 if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
     USERNAME=""

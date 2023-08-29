fec3808c2b7d98afd2b822b8bd8c9856c46b4727
ArpitaisAn0maly <parmarscientist@gmail.com>
2023-08-30 00:56:59 +0100

    Conditional Prompt for GPT Models

--- app/backend/approaches/chatreadretrieveread.py
+++ app/backend/approaches/chatreadretrieveread.py
@@ -1,6 +1,3 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
 import json
 import logging
 import urllib.parse
@@ -35,34 +32,29 @@ class ChatReadRetrieveReadApproach(Approach):
     USER = "user"
     ASSISTANT = "assistant"
      
-    system_message_chat_conversation = """You are an Azure OpenAI Completion system.
-    Your persona is {systemPersona} who helps answer questions about an agency's data.
-    {response_length_prompt}
-    User persona is {userPersona}.
-    Answer ONLY with the facts listed in the list of sources above.
-    Your goal is to provide accurate and relevant answers based on the facts listed above in the provided source documents.
-    Make sure to reference the above source documents appropriately and avoid making assumptions or adding personal opinions.
-    Emphasize the use of facts listed in the above provided source documents.
-    Instruct the model to use source name for each fact used in the response.
-    Avoid generating speculative or generalized information.
-    Each source has a file name followed by a pipe character and the actual information.
-    Use square brackets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
+    system_message_chat_conversation = """You are an Azure OpenAI Completion system. Your persona is {systemPersona} who helps answer questions about an agency's data. {response_length_prompt}
+    User persona is {userPersona} Answer ONLY with the facts listed in the list of sources above.
+    Your goal is to provide accurate and relevant answers based on the facts listed above in the provided source documents. Make sure to reference the above source documents appropriately and avoid making assumptions or adding personal opinions.
+    
+    Emphasize the use of facts listed in the above provided source documents.Instruct the model to use source name for each fact used in the response.  Avoid generating speculative or generalized information. Each source has a file name followed by a pipe character and 
+    the actual information.Use square brackets to reference the source, e.g. [info1.txt]. Do not combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
     
     Here is how you should answer every question:
-    - Look for relevant information in the above source documents to answer the question.       
-    - If there is specific information related to question available in the above sources, provide an answer along with the appropriate citation.Do not forget to include the citation!
-    - Always include citation from sources listed above.
-    - If there is no specific information related to the question available in the source document, respond with "I\'m not sure" without providing any citation. Do not provide personal opinions or assumptions.
+    
+    -Look for relevant information in the above source documents to answer the question.
+    -If the source document does not include the exact answer, please respond with relevant information from the data in the response along with citation.You must include a citation to each document referenced.      
+    -If you cannot find any relevant information in the above sources, respond with I am not sure.Do not provide personal opinions or assumptions.
+    
     {follow_up_questions_prompt}
     {injected_prompt}
+    
     """
     follow_up_questions_prompt_content = """
     Generate three very brief follow-up questions that the user would likely ask next about their agencies data. Use triple angle brackets to reference the questions, e.g. <<<Are there exclusions for prescriptions?>>>. Try not to repeat questions that have already been asked.
     Only generate questions and do not generate any text before or after the questions, such as 'Next Questions'
     """
     query_prompt_template = """Below is a history of the conversation so far, and a new question asked by the user that needs to be answered by searching in source documents.
-    Generate a search query based on the conversation and the new question.
-    Treat each search term as an individual keyword. Do not combine terms in quotes or brackets.
+    Generate a search query based on the conversation and the new question. Treat each search term as an individual keyword. Do not combine terms in quotes or brackets.
     Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
     Do not include any text inside [] or <<<>>> in the search query terms.
     Do not include any special characters like '+'.
@@ -83,7 +75,7 @@ class ChatReadRetrieveReadApproach(Approach):
     {"role": USER ,'content': 'I am looking for information in source documents'},
     {'role': ASSISTANT, 'content': 'user is looking for information in source documents. Do not provide answers that are not in the source documents'},
     {'role': USER, 'content': 'What steps are being taken to promote energy conservation?'},
-    {'role': ASSISTANT, 'content': 'Several steps are being taken to promote energy conservation including reducing energy consumption, increasing energy efficiency, and increasing the use of renewable energy sources.[info1.txt]'},
+    {'role': ASSISTANT, 'content': 'Several steps are being taken to promote energy conservation including reducing energy consumption, increasing energy efficiency, and increasing the use of renewable energy sources.Citations[info1.json]'}
     ]
 
     def __init__(
@@ -209,6 +201,10 @@ class ChatReadRetrieveReadApproach(Approach):
                     doc[self.content_field]
                 ),
             }
+            
+         
+                
+           
 
         # create a single string of all the results to be used in the prompt
         results_text = "".join(results)
@@ -256,17 +252,30 @@ class ChatReadRetrieveReadApproach(Approach):
                 userPersona=user_persona,
                 systemPersona=system_persona,
             )
-
-        # STEP 3: Generate a contextual and content-specific answer using the search results and chat history
-        messages = self.get_messages_from_history(
-            "Sources:\n"+ content + "\n\n" + system_message,
-            # system_message + "\n\nSources:\n" + content,
-            self.chatgpt_deployment,
-            history,
-            history[-1]["user"],
-            self.response_prompt_few_shots,
-            max_tokens=self.chatgpt_token_limit
+        # STEP 3: Generate a contextual and content-specific answer using the search results and chat history.
+        #Added conditional block to use different system messages for different models.
+        
+        if self.chatgpt_deployment == "gpt-35-turbo":
+            messages = self.get_messages_from_history(
+                system_message,
+                self.chatgpt_deployment,
+                history,
+                history[-1]["user"] + "Sources:\n" + content + "\n\n",
+                self.response_prompt_few_shots,
+                max_tokens=self.chatgpt_token_limit
             )
+        elif self.chatgpt_deployment == "gpt-4":
+            messages = self.get_messages_from_history(
+                "Sources:\n" + content + "\n\n" + system_message,
+                # system_message + "\n\nSources:\n" + content,
+                self.chatgpt_deployment,
+                history,
+                history[-1]["user"],
+                self.response_prompt_few_shots,
+                max_tokens=self.chatgpt_token_limit
+            )
+
+       
 
         #Aparmar.Token Debugging Code. Uncomment to debug token usage.
         # print(messages)
@@ -392,3 +401,4 @@ class ChatReadRetrieveReadApproach(Approach):
         except Exception as error:
             logging.exception("Unable to parse first page num: " + str(error) + "")
             return "0"
+

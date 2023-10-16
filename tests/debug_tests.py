f7e34cd53fd7aca316930eb0fe56c9a686aa50dd
ryonsteele <ryonsteele@gmail.com>
2023-10-16 16:12:17 +0100

    Enrichment API functional tests

--- tests/debug_tests.py
+++ tests/debug_tests.py
@@ -10,11 +10,11 @@ AZURE_SEARCH_INDEX = os.environ.get("AZURE_SEARCH_INDEX")
 AZURE_SEARCH_SERVICE_KEY = os.environ.get("AZURE_SEARCH_SERVICE_KEY")
 ENRICHMENT_APPSERVICE_NAME = os.environ.get("ENRICHMENT_APPSERVICE_NAME")
 
-# subprocess.call(['python', 'run_tests.py', '--storage_account_connection_str', BLOB_CONNECTION_STRING, \
-#     '--search_service_endpoint', AZURE_SEARCH_SERVICE_ENDPOINT, \
-#     '--search_index', AZURE_SEARCH_INDEX, \
-#     '--search_key', AZURE_SEARCH_SERVICE_KEY, \
-#     '--wait_time_seconds', '60', \
-#     '--file_extensions', 'docx', 'pdf', 'html', 'jpg', 'png', 'csv', 'md', 'pptx', 'txt', 'xlsx', 'xml'])
+subprocess.call(['python', 'run_tests.py', '--storage_account_connection_str', BLOB_CONNECTION_STRING, \
+    '--search_service_endpoint', AZURE_SEARCH_SERVICE_ENDPOINT, \
+    '--search_index', AZURE_SEARCH_INDEX, \
+    '--search_key', AZURE_SEARCH_SERVICE_KEY, \
+    '--wait_time_seconds', '60', \
+    '--file_extensions', 'docx', 'pdf', 'html', 'jpg', 'png', 'csv', 'md', 'pptx', 'txt', 'xlsx', 'xml'])
 
 subprocess.call(['python', 'run_api_tests.py', '--enrichment_service_endpoint', ENRICHMENT_APPSERVICE_NAME])

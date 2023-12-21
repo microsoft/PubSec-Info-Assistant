fd1188560b272b8e346dbd0e2696b5be165de42e
ryonsteele <ryonsteele@gmail.com>
2023-12-21 17:17:51 +0000

    Add check to remove CUA deployment if it exists

--- scripts/inf-create.sh
+++ scripts/inf-create.sh
@@ -196,6 +196,7 @@ echo $parameter_json > $DIR/../infra/main.parameters.json
 #make sure bicep is always the latest version
 az bicep upgrade
 
+#Check and Remove if exists the CUA deployment Object to resolve Bicep limitations
 az deployment sub delete --name "pid-${CUSTOMER_USAGE_ATTRIBUTION_ID}"
 
 #deploy bicep

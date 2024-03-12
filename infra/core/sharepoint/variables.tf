c541f7244143df569aa069c23108d99e532a9461
KronemeyerJoshua <v-joshuakr@microsoft.com>
2024-03-12 12:07:09 +0000

    Sharepoint Ingestion Variables

---
+++ infra/core/sharepoint/variables.tf
@@ -0,0 +1,32 @@
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "key_vault_id" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "resource_group_id" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "random_string" {
+  type = string
+}
+
+variable "tags" {}
+
+variable "subscription_id" {
+  type = string
+}
+
+

17df97023b1be172ee5d9bd92863c6a6adc1d2f0
dayland <dayland@microsoft.com>
2024-03-14 15:06:12 +0000

    Add Azure Government Cloud support and update login credentials

--- scripts/inf-create.sh
+++ scripts/inf-create.sh
@@ -49,6 +49,17 @@ if [ -n "${IN_AUTOMATION}" ]; then
   export TF_VAR_aadMgmtClientSecret=$aadMgmtAppSecret
 fi
 
+if [ -n "${IN_AUTOMATION}" ]
+then
+
+    if [ -n "${AZURE_ENVIRONMENT}" ] && $AZURE_ENVIRONMENT == "AzureUSGovernment"; then
+        az cloud set --name AzureUSGovernment 
+    fi
+
+    az login --service-principal -u "$ARM_CLIENT_ID" -p "$ARM_CLIENT_SECRET" --tenant "$ARM_TENANT_ID"
+    az account set -s "$ARM_SUBSCRIPTION_ID"
+fi
+
 # Initialise Terraform with the correct path
 ${DIR}/terraform-init.sh "$DIR/../infra/"
 

1e0a1c0512976f4c3510e6f7c33677c5908d4838
mausolf <jeffmausolf@microsoft.com>
2024-03-16 00:16:34 +0000

    removed TagsDB from outputs.tf

--- infra/core/db/outputs.tf
+++ infra/core/db/outputs.tf
@@ -9,11 +9,3 @@ output "CosmosDBLogDatabaseName" {
 output "CosmosDBLogContainerName" {
   value = azurerm_cosmosdb_sql_container.log_container.name
 }
-
-output "CosmosDBTagsDatabaseName" {
-  value = azurerm_cosmosdb_sql_database.tag_database.name
-}
-
-output "CosmosDBTagsContainerName" {
-  value = azurerm_cosmosdb_sql_container.tag_container.name
-}

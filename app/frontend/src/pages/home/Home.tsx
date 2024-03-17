6b342fa3d8cdf718f8866ed7501ecbae313d941a
Cenon Ebora <cenon4dno@gmail.com>
2024-03-17 13:44:40 +0000

    minor homepage content updates

--- app/frontend/src/pages/home/Home.tsx
+++ app/frontend/src/pages/home/Home.tsx
@@ -2,6 +2,7 @@
 // Licensed under the MIT license.
 
 import { useState } from 'react';
+import aiimage from "../../assets/ai.jfif";
 import { Pivot,
     PivotItem } from "@fluentui/react";
 
@@ -16,6 +17,7 @@ export interface IButtonExampleProps {
 const Home = () => {
         return (
         <div className={styles.contentArea} >
+            <p><img src={aiimage} /></p>
             <p>This industry accelerator showcases integration between Azure and OpenAI's large language models. It leverages Azure AI Search for data retrieval and ChatGPT-style Q&A interactions. Using the Retrieval Augmented Generation (RAG) design pattern with Azure Open AI's GPT models, it provides a natural language interaction to discover relevant responses to user queries. Azure AI Search simplifies data ingestion, transformation, indexing, and multilingual translation.</p>
             <p>The accelerator adapts prompts based on the model type for enhanced performance. Users can customize settings like temperature and persona for personalized AI interactions. It offers features like explainable thought processes, referenceable citations, and direct content for verification.</p>
             <p>Please <a href='https://aka.ms/InfoAssist/video' target='_blank'>see this video</a> for use cases that may be achievable with this accelerator.</p>

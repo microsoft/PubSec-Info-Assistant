6ee07806c4bfc5d621f3e8824886389bab9b5db8
dayland <dayland@microsoft.com>
2024-03-17 21:14:53 +0000

    Remove unused CosmosDB outputs and add sensitive storage account access key output

--- infra/core/storage/outputs.tf
+++ infra/core/storage/outputs.tf
@@ -9,3 +9,8 @@ output "primary_endpoints" {
 output "id" {
   value = azurerm_storage_account.storage.id
 }
+
+output "storage_account_access_key" {
+  value     = azurerm_storage_account.storage.primary_access_key
+  sensitive = true
+}

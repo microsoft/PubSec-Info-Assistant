ba1f3c59253c92a993e78b9ef83715edab3ef250
George Earl <geearl@microsoft.com>
2024-03-19 14:05:20 +0000

    Update inf-upgrade.sh script to initialize Terraform and import existing resources into the state

--- scripts/inf-upgrade.sh
+++ scripts/inf-upgrade.sh
@@ -12,28 +12,26 @@ source "${DIR}/load-env.sh"
 source "${DIR}/prepare-tf-variables.sh"
 pushd "$DIR/../infra" > /dev/null
 
-
-# reset the current directory on exit using a trap so that the directory is reset even on error
-function finish {
-  popd > /dev/null
-}
-trap finish EXIT
-
-# Initialise Terraform with the correct path
-${DIR}/terraform-init.sh "$DIR/../infra/"
+echo "Current Folder: $(basename "$(pwd)")"
+echo "state file: terraform.tfstate.d/${TF_VAR_environmentName}/terraform.tfstate"
+# Initialise Terraform with the correct path and clean up prior tries
+# ${DIR}/terraform-init.sh "$DIR/../infra/"
+[ -f ".terraform.lock.hcl" ] && rm ".terraform.lock.hcl"
+[ -f "terraform.tfstate.d/$TF_VAR_environmentName/terraform.tfstate" ] && rm -r "terraform.tfstate.d/$TF_VAR_environmentName/terraform.tfstate"
+terraform init -upgrade
 
 echo "TF_VAR_subscriptionId: $TF_VAR_subscriptionId"
 echo "TF_VAR_resource_group_name: $TF_VAR_resource_group_name"
 echo "TF_VAR_environmentName: $TF_VAR_environmentName"
 
-# Import the existing resources into the Terraform state
-# terraform import azurerm_resource_group.rg /subscriptions/$TF_VAR_subscriptionId/resourceGroups/$TF_VAR_resource_group_name
+# # Import the existing resources into the Terraform state
+#terraform import azurerm_resource_group.rg /subscriptions/$TF_VAR_subscriptionId/resourceGroups/$TF_VAR_resource_group_name
 
-#Storage
-cd core/storage
+# Storage
 export TF_VAR_keyVaultId="infoasst-kv-$TF_VAR_environmentName"
 export TF_VAR_name="infoasststor$TF_VAR_environmentName"
-terraform import azurerm_storage_account.storage /subscriptions/$TF_VAR_subscriptionId/resourceGroups/$TF_VAR_resource_group_name/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name
+terraform import module.storage.azurerm_storage_account.storage /subscriptions/$TF_VAR_subscriptionId/resourceGroups/$TF_VAR_resource_group_name/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name
+#terraform -chdir=core/storage import azurerm_storage_account.storage /subscriptions/$TF_VAR_subscriptionId/resourceGroups/$TF_VAR_resource_group_name/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name
 
 
 

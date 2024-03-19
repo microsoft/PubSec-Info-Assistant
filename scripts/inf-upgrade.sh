fa8f6609499575178060a3f1ead94a9f87533268
George Earl <geearl@microsoft.com>
2024-03-19 19:24:27 +0000

    Refactor import_resource_if_needed function and update Terraform initialization script

--- scripts/inf-upgrade.sh
+++ scripts/inf-upgrade.sh
@@ -11,12 +11,21 @@ import_resource_if_needed() {
     local module_path=$1
     local resource_id=$2
 
-    # Check if the resource is already managed by Terraform
-    if terraform state list "$module_path" | grep -q "$module_path"; then
-        echo "Resource $module_path is already managed by Terraform"
-    else
-        echo "Importing $module_path"
-        terraform import "$module_path" "$resource_id"
+    if [ ! -f "terraform.tfstate.d/$TF_VAR_environmentName/terraform.tfstate" ]; then
+      # The RG is not managed by Terraform
+      echo "Deployment $TF_VAR_environmentName is not managed by Terraform. Importing $module_path"
+      echo "Importing $module_path"
+      terraform import -state="terraform.tfstate.d/${TF_VAR_environmentName}/terraform.tfstate" "$module_path" "$resource_id"
+    elif terraform state list | grep -q "$module_path"; then
+      # the module is already managed by terraform
+      echo "Resource $module_path is already managed by Terraform"
+    else  
+      # the module is not managed by terraform
+      echo "Importing $module_path"
+      #terraform import -state="terraform.tfstate.d/${TF_VAR_environmentName}/terraform.tfstate" "$module_path" "$resource_id"
+      echo "TF_VAR_environmentName: $TF_VAR_environmentName"
+      echo "module_path: $module_path"
+      echo "resource_id: $resource_id"
     fi
 }
 
@@ -30,7 +39,7 @@ pushd "$DIR/../infra" > /dev/null
 echo "Current Folder: $(basename "$(pwd)")"
 echo "state file: terraform.tfstate.d/${TF_VAR_environmentName}/terraform.tfstate"
 # Initialise Terraform with the correct path and clean up prior tries
-# ${DIR}/terraform-init.sh "$DIR/../infra/"
+${DIR}/terraform-init.sh "$DIR/../infra/"
 [ -f ".terraform.lock.hcl" ] && rm ".terraform.lock.hcl"
 [ -f "terraform.tfstate.d/$TF_VAR_environmentName/terraform.tfstate" ] && rm -r "terraform.tfstate.d/$TF_VAR_environmentName/terraform.tfstate"
 terraform init -upgrade
@@ -58,16 +67,15 @@ fi
 echo
 echo -e "\e[1;32m Resource Group \e[0m"
 resourceId="/subscriptions/$TF_VAR_subscriptionId/resourceGroups/$TF_VAR_resource_group_name"
-import_resource_if_needed "module.storage.azurerm_storage_account.storage" "$resourceId"
-
+import_resource_if_needed "azurerm_resource_group.rg" "$resourceId"
 
 # Storage
-echo
-echo -e "\e[1;32m Storage \e[0m"
-export TF_VAR_keyVaultId="infoasst-kv-$random_text"
-export TF_VAR_name="infoasststore$random_text"
-resourceId="/subscriptions/$TF_VAR_subscriptionId/resourceGroups/$TF_VAR_resource_group_name/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name"
-import_resource_if_needed "module.storage.azurerm_storage_account.storage" "$resourceId"
+# echo
+# echo -e "\e[1;32m Storage \e[0m"
+# export TF_VAR_keyVaultId="infoasst-kv-$random_text"
+# export TF_VAR_name="infoasststore$random_text"
+# resourceId="/subscriptions/$TF_VAR_subscriptionId/resourceGroups/$TF_VAR_resource_group_name/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name"
+# import_resource_if_needed "module.storage.azurerm_storage_account.storage" "$resourceId"
 
 
 
@@ -76,5 +84,5 @@ import_resource_if_needed "module.storage.azurerm_storage_account.storage" "$res
 
 
 
-# STUFF
-#terraform import module.storage.azurerm_storage_account.storage /subscriptions/$TF_VAR_subscriptionId/resourceGroups/$TF_VAR_resource_group_name/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name
+# # STUFF
+# #terraform import module.storage.azurerm_storage_account.storage /subscriptions/$TF_VAR_subscriptionId/resourceGroups/$TF_VAR_resource_group_name/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name

6390594d550ab6f51c03132e1193f86078c9f5d1
George Earl <geearl@microsoft.com>
2024-03-20 21:36:50 +0000

    Fix bug in login functionality

--- scripts/inf-upgrade.sh
+++ scripts/inf-upgrade.sh
@@ -6,28 +6,31 @@ set -e
 
 figlet Upgrade
 
+# escape special chars in a string
+escape_string() {
+    echo "$1" | sed 's/[][]/\\&/g'
+}
+
 # function to issue the import statement if the resource is not managed by Terraform
 import_resource_if_needed() {
     local module_path=$1
     local resource_id=$2
 
+    # endcode the $module_path
+    module_path_escaped=$(escape_string "$module_path") 
+
     if [ ! -f "terraform.tfstate.d/$TF_VAR_environmentName/terraform.tfstate" ]; then
       # The RG is not managed by Terraform
-      figlet "RG not managed by Terraform"
-      echo "Deployment $TF_VAR_environmentName is not managed by Terraform. Importing $module_path"
-      echo "Importing $module_path"
+      echo -e "\e[34mDeployment $TF_VAR_environmentName is not managed by Terraform. Importing $module_path\e[0m"
       terraform import "$module_path" "$resource_id"
-    elif terraform state list | grep -q "$module_path"; then
-      figlet "Managed by Terraform"
-      # the module is already managed by terraform
-      echo "Resource $module_path is already managed by Terraform"
-    else  
+    elif terraform state list | grep -q $module_path_escaped; then
+      echo -e "\e[34mResource $module_path is already managed by Terraform\e[0m"
+    else 
       # the module is not managed by terraform
-      figlet "Not managed by Terraform"
-      echo "Importing $module_path"
+      echo -e "\e[34mResource $module_path is not managed by Terraform. Importing $module_path\e[0m"
       terraform import "$module_path" "$resource_id"
-
     fi
+
 }
 
 
@@ -36,13 +39,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "${DIR}/load-env.sh"
 source "${DIR}/prepare-tf-variables.sh"
 pushd "$DIR/../infra" > /dev/null
-
 echo "Current Folder: $(basename "$(pwd)")"
 echo "state file: terraform.tfstate.d/${TF_VAR_environmentName}/terraform.tfstate"
-# Initialise Terraform
-terraform init -upgrade
 
+# Initialise Terraform with the correct path
+${DIR}/terraform-init.sh "$DIR/../infra/"
 echo
+
 # Retrieve vars
 for var in "${!TF_VAR_@}"; do
     echo "\$TF_VAR_${var#TF_VAR_} = ${!var}"
@@ -59,36 +62,61 @@ else
 fi
 
 
+# ***********************************************************
 # Import the existing resources into the Terraform state
-resourceId="/subscriptions/$TF_VAR_subscriptionId/resourceGroups/$TF_VAR_resource_group_name"
-
+# ***********************************************************
 
 # Resource Group
 echo
-echo -e "\e[1;32m Resource Group \e[0m"
+figlet "Resource Group"
+resourceId="/subscriptions/$TF_VAR_subscriptionId/resourceGroups/$TF_VAR_resource_group_name"
 import_resource_if_needed "azurerm_resource_group.rg" "$resourceId"
 # providers="/providers/Microsoft.Resources/deployments/pid-$random_text"
 # import_resource_if_needed "azurerm_resource_group_template_deployment.customer_attribution" "$resourceId$providers"
 
-# Entra
-echo
-echo -e "\e[1;32m Entra \e[0m"
 
 
-# Storage
+# Entra 
 echo
-echo -e "\e[1;32m Storage \e[0m"
+figlet "Entra"
+
+
+
+# Storage 
+echo
+figlet "Storage"
 export TF_VAR_keyVaultId="infoasst-kv-$random_text"
 export TF_VAR_name="infoasststore$random_text"
-
 
 echo "TF_VAR_name: " $TF_VAR_name
 providers="/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name"
 import_resource_if_needed "module.storage.azurerm_storage_account.storage" "$resourceId$providers"
 
-url="https://$TF_VAR_name.blob.core.windows.net/upload"
-# terraform import module.storage.azurerm_storage_container.container[0] https://infoasststored51xg.blob.core.windows.net/upload
+url="https://$TF_VAR_name.blob.core.windows.net/content"
 import_resource_if_needed "module.storage.azurerm_storage_container.container[0]" "$url"
+url="https://$TF_VAR_name.blob.core.windows.net/website"
+import_resource_if_needed "module.storage.azurerm_storage_container.container[1]" "$url"
+url="https://$TF_VAR_name.blob.core.windows.net/upload"
+import_resource_if_needed "module.storage.azurerm_storage_container.container[2]" "$url"
+url="https://$TF_VAR_name.blob.core.windows.net/function"
+import_resource_if_needed "module.storage.azurerm_storage_container.container[3]" "$url"
+url="https://$TF_VAR_name.blob.core.windows.net/logs"
+import_resource_if_needed "module.storage.azurerm_storage_container.container[4]" "$url"
+
+url="https://$TF_VAR_name..queue.core.windows.net/pdf-submit-queue"
+import_resource_if_needed "module.storage.azurerm_storage_queue.queue[0]" "$url"
+url="https://$TF_VAR_name..queue.core.windows.net/pdf-polling-queue"
+import_resource_if_needed "module.storage.azurerm_storage_queue.queue[1]" "$url"
+url="https://$TF_VAR_name..queue.core.windows.net/non-pdf-submit-queue"
+import_resource_if_needed "module.storage.azurerm_storage_queue.queue[2]" "$url"
+url="https://$TF_VAR_name..queue.core.windows.net/media-submit-queue"
+import_resource_if_needed "module.storage.azurerm_storage_queue.queue[3]" "$url"
+url="https://$TF_VAR_name..queue.core.windows.net/text-enrichment-queue"
+import_resource_if_needed "module.storage.azurerm_storage_queue.queue[4]" "$url"
+url="https://$TF_VAR_name..queue.core.windows.net/image-enrichment-queue"
+import_resource_if_needed "module.storage.azurerm_storage_queue.queue[5]" "$url"
+url="https://$TF_VAR_name..queue.core.windows.net/embeddings-queue"
+import_resource_if_needed "module.storage.azurerm_storage_queue.queue[6]" "$url"
 
 
 
@@ -97,42 +125,40 @@ import_resource_if_needed "module.storage.azurerm_storage_container.container[0]
 
 
 
-
-
-# import_resource_if_needed "module.storage.azurerm_storage_container.container[1]" "$resourceId$providers"
-# providers="/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name/blobServices/default/containers/content"
-# import_resource_if_needed "module.storage.azurerm_storage_container.container" "$resourceId$providers"
-# providers="/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name/blobServices/default/containers/function"
-# import_resource_if_needed "module.storage.azurerm_storage_container.container" "$resourceId$providers"
-# providers="/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name/blobServices/default/containers/logs"
-# import_resource_if_needed "module.storage.azurerm_storage_container.container" "$resourceId$providers"
-# providers="/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name/blobServices/default/containers/website"
-# import_resource_if_needed "module.storage.azurerm_storage_container.container" "$resourceId$providers"
+# # import_resource_if_needed "module.storage.azurerm_storage_container.container[1]" "$resourceId$providers"
+# # providers="/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name/blobServices/default/containers/content"
+# # import_resource_if_needed "module.storage.azurerm_storage_container.container" "$resourceId$providers"
+# # providers="/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name/blobServices/default/containers/function"
+# # import_resource_if_needed "module.storage.azurerm_storage_container.container" "$resourceId$providers"
+# # providers="/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name/blobServices/default/containers/logs"
+# # import_resource_if_needed "module.storage.azurerm_storage_container.container" "$resourceId$providers"
+# # providers="/providers/Microsoft.Storage/storageAccounts/$TF_VAR_name/blobServices/default/containers/website"
+# # import_resource_if_needed "module.storage.azurerm_storage_container.container" "$resourceId$providers"
 
 
 
 
-# providers="/providers/Microsoft.KeyVault/vaults/infoasst-kv-$random_text/secrets/BLOB-CONNECTION-STRING"
-# import_resource_if_needed "module.storage.azurerm_key_vault_secret.storage_connection_string" "$resourceId$providers"
+# # providers="/providers/Microsoft.KeyVault/vaults/infoasst-kv-$random_text/secrets/BLOB-CONNECTION-STRING"
+# # import_resource_if_needed "module.storage.azurerm_key_vault_secret.storage_connection_string" "$resourceId$providers"
 
-# providers="/providers/Microsoft.KeyVault/vaults/infoasst-kv-$random_text/secrets/AZURE-BLOB-STORAGE-KEY"
-# import_resource_if_needed "module.storage.azurerm_key_vault_secret.storage_key" "$resourceId$providers"
+# # providers="/providers/Microsoft.KeyVault/vaults/infoasst-kv-$random_text/secrets/AZURE-BLOB-STORAGE-KEY"
+# # import_resource_if_needed "module.storage.azurerm_key_vault_secret.storage_key" "$resourceId$providers"
 
 
-# # Key vault
-# echo
-# echo -e "\e[1;32m Key vault \e[0m"
-# export TF_VAR_keyVaultId="infoasst-kv-$random_text"
-# export TF_VAR_name="infoasst-cosmos-$random_text"
+# # # Key vault
+# # echo
+# # echo -e "\e[1;32m Key vault \e[0m"
+# # export TF_VAR_keyVaultId="infoasst-kv-$random_text"
+# # export TF_VAR_name="infoasst-cosmos-$random_text"
 
-# providers="/providers/Microsoft.DocumentDB/databaseAccounts/$TF_VAR_name"
-# import_resource_if_needed "module.cosmosdb.azurerm_cosmosdb_account.cosmosdb_account" "$resourceId$providers"
+# # providers="/providers/Microsoft.DocumentDB/databaseAccounts/$TF_VAR_name"
+# # import_resource_if_needed "module.cosmosdb.azurerm_cosmosdb_account.cosmosdb_account" "$resourceId$providers"
 
-# providers="/providers/Microsoft.DocumentDB/databaseAccounts/$TF_VAR_name/sqlDatabases/statusdb"
-# import_resource_if_needed "module.cosmosdb.azurerm_cosmosdb_sql_database.log_database" "$resourceId$providers"
+# # providers="/providers/Microsoft.DocumentDB/databaseAccounts/$TF_VAR_name/sqlDatabases/statusdb"
+# # import_resource_if_needed "module.cosmosdb.azurerm_cosmosdb_sql_database.log_database" "$resourceId$providers"
 
-# providers="/providers/Microsoft.DocumentDB/databaseAccounts/$TF_VAR_name/sqlDatabases/tagdb"
-# import_resource_if_needed "module.cosmosdb.azurerm_cosmosdb_sql_database.log_database" "$resourceId$providers"
+# # providers="/providers/Microsoft.DocumentDB/databaseAccounts/$TF_VAR_name/sqlDatabases/tagdb"
+# # import_resource_if_needed "module.cosmosdb.azurerm_cosmosdb_sql_database.log_database" "$resourceId$providers"
 
-# providers="/providers/Microsoft.KeyVault/vaults/infoasst-kv-$random_text/secrets/COSMOSDB-KEY"
-# import_resource_if_needed "module.cosmosdb.azurerm_key_vault_secret.search_service_key" "$resourceId$providers"
+# # providers="/providers/Microsoft.KeyVault/vaults/infoasst-kv-$random_text/secrets/COSMOSDB-KEY"
+# # import_resource_if_needed "module.cosmosdb.azurerm_key_vault_secret.search_service_key" "$resourceId$providers"

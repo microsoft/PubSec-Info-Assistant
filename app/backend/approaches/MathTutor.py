4d8fb89c87ff89f140061030f29abf2754605818
William Otey <wko0001@auburn.edu>
2024-03-21 15:37:10 +0000

    update tutor to only create one model

--- app/backend/approaches/MathTutor.py
+++ app/backend/approaches/MathTutor.py
@@ -200,10 +200,7 @@ def process_agent_response( question):
     #         yield {"data": f'Final Output: {chunk["output"]}'}
 
 #Function to process clues
-def generate_response(question):
-    model = AzureChatOpenAI(
-        openai_api_version=OPENAI_API_VERSION ,
-        deployment_name=OPENAI_DEPLOYMENT_NAME)   
+def generate_response(question):  
     prompt_template = ChatPromptTemplate.from_template(template=prompt)
     messages = prompt_template.format_messages(
     question=question

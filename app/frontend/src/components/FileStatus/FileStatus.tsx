09f126ffd4555c731a9442508ea152fc2ae6f83e
George Earl <geearl@microsoft.com>
2024-03-26 22:55:19 +0000

    Fix typo in dropdownFileStateOptions

--- app/frontend/src/components/FileStatus/FileStatus.tsx
+++ app/frontend/src/components/FileStatus/FileStatus.tsx
@@ -29,7 +29,7 @@ const dropdownTimespanOptions = [
 const dropdownFileStateOptions = [
     { key: 'FileStates', text: 'File States', itemType: DropdownMenuItemType.Header },
     { key: FileState.All, text: 'All' },
-    { key: FileState.Complete, text: 'Completed' },
+    { key: FileState.Complete, text: 'Complete' },
     { key: FileState.Error, text: 'Error' },
     { key: FileState.Processing, text: 'Processing' },
     { key: FileState.Indexing, text: 'Indexing' },

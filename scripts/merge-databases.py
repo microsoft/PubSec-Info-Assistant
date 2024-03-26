8a545951713be782add0c4c1bb20329c5fda007f
KronemeyerJoshua <v-joshuakr@microsoft.com>
2024-03-26 16:50:58 +0000

    Refactored and updated code to reflect one cosmos

--- scripts/merge-databases.py
+++ scripts/merge-databases.py
@@ -5,15 +5,14 @@
 #
 # To use this script, you need to:
 # 1. Have run make deploy to create a new IA resource group
-# 2. Copy over the inf_output.json file from the output folder of the 1.0 deployment
-#    to the scripts folder of the 1.1 deployment and rename if to in_output_old.json
-# 3. run make run-migration to perfomr the migration
+# 2. Ensure you have your infra_output.json file in root of your project (where package.json is)
+#     This is where we will get information about your current bicep deployment
+# 3. run make run-migration to perform the migration
 #
 # The script will:
-# 1. Read required values from in_output.json
-# 2. Read required values from in_output_old.json
-# 3. Read required secrets from azure keyvault
-# 4. Migrate Cosmos DB tags from the old tags container and database to the
+# 1. Read required values from infra_output.json
+# 2. Read required secrets from azure keyvault
+# 3. Migrate Cosmos DB tags from the old tags container and database to the
 
 
 from pyfiglet import Figlet
@@ -31,40 +30,24 @@ credential = DefaultAzureCredential()
 
 
 # *************************************************************************
-# Read required NEW values from in_output.json & new key vault
-print("Reading values from inf_output.json")
-with open('inf_output.json', 'r') as file:
-    inf_output = json.load(file)
-    
-cosmosdb_url_new = inf_output['AZURE_COSMOSDB_URL']['value']
-key_vault_name_new = inf_output['DEPLOYMENT_KEYVAULT_NAME']['value']
-key_vault_url_new = "https://" + key_vault_name_new + ".vault.azure.net/"
-
-client_new = SecretClient(vault_url=key_vault_url_new, credential=credential) 
-cosmosdb_key_new = client_new.get_secret('COSMOSDB-KEY') 
-# *************************************************************************
-
-
-# *************************************************************************
-# Read required OLD values from in_output_old.json & old key vault
+# Read required values from infra_output.json & new key vault
 print("Reading values from infra_output.json")
 with open('infra_output.json', 'r') as file:
     inf_output = json.load(file)
     
-cosmosdb_url_old = inf_output['properties']['outputs']['azurE_COSMOSDB_URL']['value']
-key_vault_name_old = inf_output['properties']['outputs']['deploymenT_KEYVAULT_NAME']['value']
-key_vault_url_old = "https://" + key_vault_name_old + ".vault.azure.net/"
+cosmosdb_url = inf_output['properties']['outputs']['azurE_COSMOSDB_URL']['value']
+key_vault_name = inf_output['properties']['outputs']['deploymenT_KEYVAULT_NAME']['value']
+key_vault_url = "https://" + key_vault_name + ".vault.azure.net/"
 
-# Read required secrets from azure keyvault
-client_old = SecretClient(vault_url=key_vault_url_old, credential=credential) 
-cosmosdb_key_old = client_old.get_secret('COSMOSDB-KEY')
+client = SecretClient(vault_url=key_vault_url, credential=credential) 
+cosmosdb_key = client.get_secret('COSMOSDB-KEY') 
 # *************************************************************************
 
 
 # *************************************************************************
 # Migrate Cosmos DB tags from the old tags container and database to the
 # status container and database as these have now been merged
-client = CosmosClient(cosmosdb_url_old, cosmosdb_key_old.value)
+client = CosmosClient(cosmosdb_url, cosmosdb_key.value)
 
 try:
     # Get old status docs
@@ -90,10 +73,9 @@ try:
     for item in old_status_items:
         if item['id'] in tags_dict:
             item['tags'] = tags_dict[item['id']]
-            item['migration_log'] = [f'Migrated tags & status from {cosmosdb_url_old} old tags container to status container']
+            item['migration_log'] = [f'Migrated tags & status from {cosmosdb_url} old tags container to status container']
         
-    # Write merged json documents to the new cosmos database
-    client = CosmosClient(cosmosdb_url_new, credential=cosmosdb_key_new.value)
+    # Write merged json documents to statuscontainer
     database_new = client.get_database_client('statusdb')
     container_new = database_new.get_container_client('statuscontainer')
     for item in old_status_items:

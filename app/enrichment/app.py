c74af09f1d2f8198273aa86ff9527eff3f0a69d0
Nehemiah Kuhns <85817913+nhwkuhns@users.noreply.github.com>
2024-04-02 11:18:25 -0500

    Update app.py
    added `.lower()` to ensure the str read is in correctly converted to a bool.

--- app/enrichment/app.py
+++ app/enrichment/app.py
@@ -74,7 +74,7 @@ for key, value in ENV.items():
     
 search_creds = AzureKeyCredential(ENV["AZURE_SEARCH_SERVICE_KEY"])
 
-if str_to_bool.get(ENV["IS_GOV_CLOUD_DEPLOYMENT"]):
+if str_to_bool.get(ENV["IS_GOV_CLOUD_DEPLOYMENT"].lower()):
     openai.api_base = "https://" + ENV["AZURE_OPENAI_SERVICE"] + ".openai.azure.us/"
 else:
     openai.api_base = "https://" + ENV["AZURE_OPENAI_SERVICE"] + ".openai.azure.com/"

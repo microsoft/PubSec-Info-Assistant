8037e02dba2c9606d9c2b7bf82ac9f66f04ab2ef
William Otey <wko0001@auburn.edu>
2024-04-04 02:17:45 +0100

    take out you're friendly

--- app/frontend/src/pages/tutor/Tutor.tsx
+++ app/frontend/src/pages/tutor/Tutor.tsx
@@ -72,7 +72,7 @@ const Tutor = () => {
     };
 return (
     <div>
-    <h1 className={styles.title}>Your Friendly Math Assistant</h1>
+    <h1 className={styles.title}>Math Assistant</h1>
     <div className={styles.centeredContainer}>
         <form className={styles.formClass} onSubmit={handleInput}>
             <p className={styles.inputLabel}>Enter question:</p>

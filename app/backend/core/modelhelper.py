e1fb0a971f594394d7fa4980333c0eab1d5d48f4
dayland <48474707+dayland@users.noreply.github.com>
2024-05-29 16:00:15 +0100

    chore: Update tiktoken dependency to version 0.7.0 to support gpt-4o

--- app/backend/core/modelhelper.py
+++ app/backend/core/modelhelper.py
@@ -8,12 +8,14 @@ MODELS_2_TOKEN_LIMITS = {
     "gpt-35-turbo-16k": 16385,
     "gpt-3.5-turbo-16k": 16385,
     "gpt-4": 8192,
-    "gpt-4-32k": 32768
+    "gpt-4-32k": 32768,
+    "gpt-4o": 128000
 }
 
 AOAI_2_OAI = {
     "gpt-35-turbo": "gpt-3.5-turbo",
-    "gpt-35-turbo-16k": "gpt-3.5-turbo-16k"
+    "gpt-35-turbo-16k": "gpt-3.5-turbo-16k",
+    "gpt-4o": "gpt-4o"
 }
 
 

0f1061ad21458d6c279458e56a04cd3b53c39ed8
dayland <48474707+dayland@users.noreply.github.com>
2024-08-28 15:13:35 +0100

    updated folder-picker to use "getfolders" api instead of direct storage account access

--- app/frontend/src/api/api.ts
+++ app/frontend/src/api/api.ts
@@ -3,7 +3,6 @@
 
 import { ChatResponse, 
     ChatRequest, 
-    BlobClientUrlResponse, 
     AllFilesUploadStatus, 
     GetUploadStatusRequest, 
     GetInfoResponse, 
@@ -62,22 +61,6 @@ export async function chatApi(options: ChatRequest, signal: AbortSignal): Promis
 
 export function getCitationFilePath(citation: string): string {
     return `${encodeURIComponent(citation)}`;
-}
-
-export async function getBlobClientUrl(): Promise<string> {
-    const response = await fetch("/getblobclienturl", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    });
-
-    const parsedResponse: BlobClientUrlResponse = await response.json();
-    if (response.status > 299 || !response.ok) {
-        throw Error(parsedResponse.error || "Unknown error");
-    }
-
-    return parsedResponse.url;
 }
 
 export async function getAllUploadStatus(options: GetUploadStatusRequest): Promise<AllFilesUploadStatus> {

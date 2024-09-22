1dc49d440890d65bca41f3d9eeb201d70f1b08bb
ruandersMSFT <russell@russellanderson.net>
2024-09-22 19:36:42 +0100

    Terraform 4.3.0

--- infra/providers.tf
+++ infra/providers.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.113.0"
+      version = "~> 4.3.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
